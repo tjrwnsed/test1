f5047755aaa0a6a8688d6735df371339d26cf407
tjrwnsed <tjrwnsing@naver.com>
2019-03-28 19:52:26 +0900

    test

--- app/src/main/java/com/doorsellman/MoonSellManMap.java
+++ app/src/main/java/com/doorsellman/MoonSellManMap.java
@@ -18,7 +18,7 @@ import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 
-public class MoonSellManMap extends BaseActivity {
+public class MoonSellManMap extends BaseActivity implements OnMapReadyCallback {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
     private final String CLIENT_ID = "3v5jdwu0zw";
@@ -30,51 +30,48 @@ public class MoonSellManMap extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.moonsellman_map_activity);
 
-//        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-//        if (mapFragment == null) {
-//            mapFragment = MapFragment.newInstance();
-//            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
-//        }
-//        mapFragment.getMapAsync(this);
-//
-//        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
 
     }
 
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-//                                           @NonNull int[] grantResults) {
-//        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-//            return;
-//        }
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
 
-//    @Override
-//    public void onMapReady(@NonNull NaverMap naverMap) {
-//
-//        naverMap.setLocationSource(locationSource);
-//        UiSettings uiSettings = naverMap.getUiSettings();
-//        uiSettings.setLocationButtonEnabled(true);
-//        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-//
-//        Button bt = (Button)findViewById(R.id.location);
-//        bt.setOnClickListener(v -> naverMap.setLocationTrackingMode(LocationTrackingMode.Follow));
-//
-//        Marker marker = new Marker();
-//        marker.setPosition(new LatLng(37.5543263, 126.97477900000001));
-//        marker.setCaptionText("세직이네");
-//        marker.setMap(naverMap);
-//        marker.setOnClickListener(overlay -> {
-//            Toast.makeText(this, "마커 1 클릭됨", Toast.LENGTH_SHORT).show();
-//            // 이벤트 전파
-//            Intent intent=new Intent(MoonSellManMap.this, MoonSellAffiliate.class);
-//            startActivity(intent);
-//            return false;
-//        });
-//    }
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5543263, 126.97477900000001));
+        marker.setCaptionText("세직이네");
+        marker.setMap(naverMap);
+        marker.setOnClickListener(overlay -> {
+            Toast.makeText(this, "마커 1 클릭됨", Toast.LENGTH_SHORT).show();
+            // 이벤트 전파
+            Intent intent=new Intent(MoonSellManMap.this, MoonSellAffiliate.class);
+            startActivity(intent);
+            return false;
+        });
+    }
 
 }
